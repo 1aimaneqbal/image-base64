fa9f928a56c6dc81eeba27ed66d33f8a56a5d9f7
1aimaneqbal <1aimaneqbal@gmail.com>
2021-06-22 18:33:24 +0530

    Bugs & Design Fixes

--- src/components/ImageComponent.js
+++ src/components/ImageComponent.js
@@ -1,42 +1,42 @@
-import React, {useState, useRef} from 'react'
+import React, { useState, useRef } from 'react'
 import { UilTimes, UilCloudUpload } from '@iconscout/react-unicons'
 
-const ImageComponent = ({image64, setImage64}) => {
+const ImageComponent = ({ image64, setImage64 }) => {
     const inputRef = useRef(null)
     const [imageName, setImageName] = useState('')
-    const imageSelectHandler = (e) =>{
+    const imageSelectHandler = (e) => {
         setImageName(e.target.files[0].name)
         encodeFileBase64(e.target.files[0])
     }
-    const clickInput = e =>{
+    const clickInput = e => {
         e.preventDefault()
         inputRef.current.click()
     }
-    const cancelSelectedImage = e =>{
+    const cancelSelectedImage = e => {
         e.preventDefault()
         setImage64("")
     }
     const encodeFileBase64 = (file) => {
-      var reader = new FileReader();
-      if (file) {
-        reader.readAsDataURL(file);
-        reader.onload = () => {
-          var Base64 = reader.result;
-          console.log(Base64);
-          setImage64(Base64);
-        };
-        reader.onerror = (error) => {
-          console.log("error: ", error);
-        };
-      }
+        var reader = new FileReader();
+        if (file) {
+            reader.readAsDataURL(file);
+            reader.onload = () => {
+                var Base64 = reader.result;
+                console.log(Base64);
+                setImage64(Base64);
+            };
+            reader.onerror = (error) => {
+                console.log("error: ", error);
+            };
+        }
     };
-    const doNothing = () =>{}
+    const doNothing = () => { }
     return (
         <div className="imageContainer">
-            <div className="imageDisplay" onClick={image64? doNothing:clickInput}>
+            <div className="imageDisplay" onClick={image64 ? doNothing : clickInput}>
                 {
                     image64 ? (
-                        <img src={image64} />
+                        <img src={image64} alt="" />
                     ) : (
                         <div className="noImageDiv">
                             <UilCloudUpload />
@@ -53,7 +53,7 @@ const ImageComponent = ({image64, setImage64}) => {
                 }
             </div>
             <div className="imageUploader">
-                <input type="file" onChange={imageSelectHandler} accept="image/*" hidden ref={inputRef}/>
+                <input type="file" onChange={imageSelectHandler} accept="image/*" hidden ref={inputRef} />
                 <button onClick={clickInput}>Select an Image</button>
             </div>
         </div>
--- src/components/TextareaComponent.js
+++ src/components/TextareaComponent.js
@@ -1,30 +1,30 @@
-import React, {useState, useRef} from 'react'
-import {UilHistory, UilCopy } from '@iconscout/react-unicons'
+import React, { useState, useRef } from 'react'
+import { UilHistory, UilCopy } from '@iconscout/react-unicons'
 
-const TextareaComponent = ({image64, setImage64}) => {
+const TextareaComponent = ({ image64, setImage64 }) => {
     const textareaRef = useRef('null')
     const [text, setText] = useState('')
-    const textAreaHandler = e =>{
+    const textAreaHandler = e => {
         setText(e.target.value)
     }
-    const copy64Handler = e =>{
+    const copy64Handler = e => {
         e.preventDefault()
-        if(image64){
+        if (image64) {
             navigator.clipboard.writeText(image64)
             alert("Copied to clipboard")
         }
     }
-    const resetHandler = e =>{
+    const resetHandler = e => {
         e.preventDefault()
         setText('')
         setImage64('')
     }
-    const imageGeneratorHandler = e =>{
+    const imageGeneratorHandler = e => {
         e.preventDefault()
-        if(text.substr(0,10)==="data:image"){
+        if (text.substr(0, 10) === "data:image") {
             setImage64(text)
         }
-        else{
+        else {
             alert("Invalid Base64 format")
         }
     }
@@ -33,9 +33,10 @@ const TextareaComponent = ({image64, setImage64}) => {
             <textarea onChange={textAreaHandler} value={text ? text : image64} ref={textareaRef} placeholder="Upload an image to automatically generate its Base64 value or paste a Base64 value here to generate an image."></textarea>
             <div className="textareabtns">
                 <button onClick={imageGeneratorHandler}>Generate Image</button>
-                <button onClick={copy64Handler}><UilCopy /> Copy</button>
-                <button onClick={resetHandler}><UilHistory /> Reset</button>
+                <button onClick={copy64Handler}><UilCopy /><span> Copy</span></button>
+                <button onClick={resetHandler}><UilHistory /><span> Reset</span></button>
             </div>
+            <h6>Copy Functionality might not work on mobile browsers.</h6>
         </div>
     )
 }
